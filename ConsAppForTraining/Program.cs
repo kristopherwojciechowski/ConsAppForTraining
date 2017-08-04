2312b2cd5b010d4e63d97dc0031a0f6e0dd6aeef
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-08-04 11:04:42 +0200

    Add project files.

---
+++ ConsAppForTraining/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppForTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give first value:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = {0}", a);
+
+            Console.WriteLine("give second value:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("b = {0}", b);
+
+            if(a<b)
+            {
+                Console.WriteLine("{0} < {1}", a,b);
+            }
+            else
+            {
+                Console.WriteLine("{0} > {1}", a, b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
