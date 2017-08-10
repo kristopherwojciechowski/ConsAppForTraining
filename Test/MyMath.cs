42f80df659aedf9c309a319aeebbae5ed7b02f58
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-08-10 22:00:28 +0200

    tset add

---
+++ Test/MyMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class MyMath
+    {
+        public static String WiekszeMniejsze(int a, int b)
+        {
+            String AA = a.ToString();
+            String BB = b.ToString();
+            if (a < b)
+            {
+                return AA + "<" + BB;
+            }
+            else
+            {
+                return AA + ">" + BB;
+            }
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 5;
+            int b = 7;
+
+            String wynik = MyMath.WiekszeMniejsze(a, b);
+
+            Assert.ReferenceEquals("5<7",wynik);
+        }
+    }
+}
