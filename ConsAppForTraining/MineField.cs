d33edb9f2628f31858e7e373d42bfd92638b1973
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-08-24 11:05:57 +0200

    attempt to define a method returning list of elements type of int[]

--- ConsAppForTraining/MineField.cs
+++ ConsAppForTraining/MineField.cs
@@ -24,5 +24,26 @@ namespace ConsAppForTraining
             }
             return new Tuple<int, int>(a, b);
         }
+        public static List<int[]> MineLocationList(int[,,] field)
+        {
+            List<int[]> elemtsList = new List<int[]>();
+
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int ii = 0; ii < field.GetLength(0); ii++)
+                {
+                    for (int iii = 0; iii < field.GetLength(0); iii++)
+                    {
+                        if (field[i, ii, iii] != 0)
+                        {
+                            int[] pozycja = new int[3] { i, ii, iii };
+                            elemtsList.Add(pozycja);
+                        }
+        
+                    }
+                }
+            }
+            return new List<int[]>(elemtsList);
+        }
     }
 }
--- ConsAppForTraining/Program.cs
+++ ConsAppForTraining/Program.cs
@@ -45,25 +45,81 @@ namespace ConsAppForTraining
 
             #region mine field
 
-            int[,] map = new int[,] { { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            //int[,] map = new int[,] { { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
 
-            Console.WriteLine(map[2, 2]);
+            //Console.WriteLine(map[2, 2]);
 
 
-            for(int i = 0; i< map.GetLength(0); i++)
+            //for(int i = 0; i< map.GetLength(0); i++)
+            //{
+            //    for(int j=0; j<map.GetLength(0); j++)
+            //    {
+            //        if (map[i, j] != 0)
+            //        {
+            //            Console.WriteLine("element [{0},{1}] jest rozny od zera", i, j);
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine("Metoda MineLocation => " + MineField.MineLocation(map));
+
+            #endregion
+
+            #region
+
+            Random rr = new Random();
+
+            int numberOfElements = rr.Next(2,10);
+            Console.WriteLine("random num = " + numberOfElements);
+            int[,,] map3D = new int[numberOfElements, numberOfElements, numberOfElements];
+
+            for (int i=0; i<map3D.GetLength(0); i++)
             {
-                for(int j=0; j<map.GetLength(0); j++)
+                for (int ii=0; ii<map3D.GetLength(0); ii++)
                 {
-                    if (map[i, j] != 0)
+                    for (int iii = 0; iii < map3D.GetLength(0); iii++)
                     {
-                        Console.WriteLine("element [{0},{1}] jest rozny od zera", i, j);
+                        map3D[i, ii, iii] = rr.Next(2);
+                        Console.WriteLine("element = {0}; x = {1}; y = {2}; z = {3}", map3D[i, ii, iii], i, ii, iii);
                     }
                 }
             }
+            int iloscElementow = 0;
+            for (int i = 0; i < map3D.GetLength(0); i++)
+            {
+                for (int ii = 0; ii < map3D.GetLength(0); ii++)
+                {
+                    for (int iii = 0; iii < map3D.GetLength(0); iii++)
+                    {
+                        if (map3D[i, ii, iii] != 0)
+                        {
+                            Console.WriteLine("element [{0},{1},{2}] jest rozny od zera", i, ii, iii);
+                            iloscElementow++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("random num = " + numberOfElements);
+            Console.WriteLine();
+            Console.WriteLine("ilosc elementów rożnych od zera: {0}", iloscElementow);
 
-            Console.WriteLine("Metoda MineLocation => " + MineField.MineLocation(map));
+            //List<int[]> elemtsList = new List<int[]>();
+
+            //for (int k = 0; k< MineField.MineLocationList(map3D).Count; k++)
+            //{
+            //    Console.WriteLine("współrzędne elementu różnego od zera: {0}", MineField.MineLocationList(map3D).);
+            //}
+            //MineField.MineLocationList(map3D);
+
+            foreach(int[] element in MineField.MineLocationList(map3D))
+            {
+                Console.WriteLine("współrzędne elementu różnego od zera: {0}", element);
+
+            }
 
             #endregion
+
             Console.ReadKey();
         }
     }
