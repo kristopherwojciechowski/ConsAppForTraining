a404209b688eb251e4bb3c157c24a5bb14fe54a1
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-08-24 18:55:47 +0200

    next solution

--- ConsAppForTraining/Program.cs
+++ ConsAppForTraining/Program.cs
@@ -65,58 +65,177 @@ namespace ConsAppForTraining
 
             #endregion
 
-            #region
+            #region 3D field with random numbers of elements
 
             Random rr = new Random();
 
-            int numberOfElements = rr.Next(2,10);
-            Console.WriteLine("random num = " + numberOfElements);
-            int[,,] map3D = new int[numberOfElements, numberOfElements, numberOfElements];
+            //int numberOfElements = rr.Next(2,10);
+            //Console.WriteLine("random num = " + numberOfElements);
+            //int[,,] map3D = new int[numberOfElements, numberOfElements, numberOfElements];
 
-            for (int i=0; i<map3D.GetLength(0); i++)
-            {
-                for (int ii=0; ii<map3D.GetLength(0); ii++)
-                {
-                    for (int iii = 0; iii < map3D.GetLength(0); iii++)
-                    {
-                        map3D[i, ii, iii] = rr.Next(2);
-                        Console.WriteLine("element = {0}; x = {1}; y = {2}; z = {3}", map3D[i, ii, iii], i, ii, iii);
-                    }
-                }
-            }
-            int iloscElementow = 0;
-            for (int i = 0; i < map3D.GetLength(0); i++)
-            {
-                for (int ii = 0; ii < map3D.GetLength(0); ii++)
-                {
-                    for (int iii = 0; iii < map3D.GetLength(0); iii++)
-                    {
-                        if (map3D[i, ii, iii] != 0)
-                        {
-                            Console.WriteLine("element [{0},{1},{2}] jest rozny od zera", i, ii, iii);
-                            iloscElementow++;
-                        }
-                    }
-                }
-            }
-            Console.WriteLine();
-            Console.WriteLine("random num = " + numberOfElements);
-            Console.WriteLine();
-            Console.WriteLine("ilosc elementów rożnych od zera: {0}", iloscElementow);
-
-            //List<int[]> elemtsList = new List<int[]>();
-
-            //for (int k = 0; k< MineField.MineLocationList(map3D).Count; k++)
+            //for (int i=0; i<map3D.GetLength(0); i++)
             //{
-            //    Console.WriteLine("współrzędne elementu różnego od zera: {0}", MineField.MineLocationList(map3D).);
+            //    for (int ii=0; ii<map3D.GetLength(0); ii++)
+            //    {
+            //        for (int iii = 0; iii < map3D.GetLength(0); iii++)
+            //        {
+            //            map3D[i, ii, iii] = rr.Next(2);
+            //            Console.WriteLine("element = {0}; x = {1}; y = {2}; z = {3}", map3D[i, ii, iii], i, ii, iii);
+            //        }
+            //    }
             //}
-            //MineField.MineLocationList(map3D);
+            //int iloscElementow = 0;
+            //for (int i = 0; i < map3D.GetLength(0); i++)
+            //{
+            //    for (int ii = 0; ii < map3D.GetLength(0); ii++)
+            //    {
+            //        for (int iii = 0; iii < map3D.GetLength(0); iii++)
+            //        {
+            //            if (map3D[i, ii, iii] != 0)
+            //            {
+            //                Console.WriteLine("element [{0},{1},{2}] jest rozny od zera", i, ii, iii);
+            //                iloscElementow++;
+            //            }
+            //        }
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("random num = " + numberOfElements);
+            //Console.WriteLine();
+            //Console.WriteLine("ilosc elementów rożnych od zera: {0}", iloscElementow);
 
-            foreach(int[] element in MineField.MineLocationList(map3D))
+            //foreach(int[] element in MineField.MineLocationList(map3D))
+            //{
+            //    Console.WriteLine("współrzędne elementu różnego od zera: {0}", element);
+
+            //}
+
+            #endregion
+
+            #region
+            int elementpoczatkowy = 0;
+            int elementInt;
+            string elementString;
+            string start = elementpoczatkowy.ToString();
+            string ciag;
+            ciag = start;
+            int n = rr.Next(999, 9999);
+            int dlugoscStringa = ciag.Length;
+
+            for (int i = 1; i < 15; i++)
             {
-                Console.WriteLine("współrzędne elementu różnego od zera: {0}", element);
+                //Console.WriteLine("iteracja: {0}", i);
+                if (i % 2 == 1)
+                {
+                    for (int k = 0; k < dlugoscStringa; k++)
+                    {
+                        //Console.WriteLine("iteracjaB: {0}", k);
 
+                        elementInt = Int16.Parse(ciag.Substring(k, 1));
+                        //Console.WriteLine("element before : {0}", element);
+
+                        int a;
+                        if (elementInt == 0)
+                        {
+                            a = 1;
+                        }
+                        else
+                        {
+                            a = 0;
+                        }
+                        //Console.WriteLine("element after : {0}", a);
+                        string cyfra = a.ToString();
+                        ciag += cyfra;
+                    }
+                }
+                else
+                {
+                    for (int m = dlugoscStringa - 1; m >= 0; m--)
+                    {
+                        elementString = ciag.Substring(m, 1);
+                        ciag += elementString;
+                    }
+                }
+                dlugoscStringa = ciag.Length;
+
+                //Console.WriteLine("finalny ciag: {0}", ciag);
+                //Console.WriteLine("ilosc elemntow w ciagu finalnym: {0}", ciag.Count());
+
+                //Console.WriteLine();
             }
+
+            //Console.WriteLine("wynik koncowy: " + ciag);
+            String rozwiazanieA = ciag.Substring(0, n);
+
+            Console.WriteLine("wylosowana liczba : " + n);
+
+            Console.WriteLine("rozwiazanie A: " + rozwiazanieA);
+            Console.WriteLine("rozwiazanie B: " + Morse.ThueMorse(n));
+
+            Console.WriteLine("ilosc elementow w stringu: " + Morse.ThueMorse(n).Length);
+
+            Console.WriteLine("ilosc elementow w stringu: " + rozwiazanieA.Length);
+
+            #endregion
+
+            #region
+            //int elementpoczatkowy = 0;
+            //int element;
+            //string start = elementpoczatkowy.ToString();
+            //string ciag;
+            //string nowyCiag;
+            //ciag = start;
+            //int n = rr.Next(999, 9999);
+
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    //Console.WriteLine("iteracja: {0}", i);
+            //    nowyCiag = "";
+
+            //    for (int k = 0; k < ciag.Length; k++)
+            //    {
+            //        //Console.WriteLine("iteracjaB: {0}", k);
+
+            //        element = Int16.Parse(ciag.Substring(k, 1));
+            //        //Console.WriteLine("element before : {0}", element);
+
+            //        int a;
+            //        if (element == 0)
+            //        {
+            //            a = 1;
+            //        }
+            //        else
+            //        {
+            //            a = 0;
+            //        }
+            //        //Console.WriteLine("element after : {0}", a);
+            //        string cyfra = a.ToString();
+            //        nowyCiag = nowyCiag + cyfra;
+
+            //        //Console.WriteLine("nowy ciag: {0}", nowyCiag);
+            //    }
+            //    int dlugoscStringa = ciag.Length;
+            //    ciag = ciag + nowyCiag;
+
+            //    //Console.WriteLine("finalny ciag: {0}", ciag);
+            //    //Console.WriteLine("ilosc elemntow w ciagu finalnym: {0}", ciag.Count());
+
+            //    //Console.WriteLine();
+
+            //}
+
+            //Console.WriteLine("wynik koncowy: " + ciag);
+            ////int koniec = rr.Next(4444,9999);
+            //String rozwiazanieA = ciag.Substring(0, n);
+
+            //Console.WriteLine("wylosowana liczba : " + n);
+
+            //Console.WriteLine("rozwiazanie A: " + rozwiazanieA);
+            ////Console.WriteLine("rozwiazanie B: " + Morse.ThueMorse(koniec));
+
+            ////Console.WriteLine("ilosc elementow w stringu: " + Morse.ThueMorse(koniec).Length);
+
+            //Console.WriteLine("ilosc elementow w stringu: " + rozwiazanieA.Length);
 
             #endregion
 
---
+++ ConsAppForTraining/ThueMorse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppForTraining
+{
+    class Morse
+    {
+        public static string MorseStringGen()
+        {
+            int element, a;
+            string ciag = "0";
+            int dlugoscStringa = ciag.Length;
+
+            for (int i = 0; i < 14; i++)
+            {
+                for (int k = 0; k < dlugoscStringa; k++)
+                {
+                    element = Int16.Parse(ciag.Substring(k, 1));
+                    if (element == 0)
+                    {
+                        a = 1;
+                    }
+                    else
+                    {
+                        a = 0;
+                    }
+                    string cyfra = a.ToString();
+                    ciag = ciag + cyfra;
+                }
+                dlugoscStringa = ciag.Length;
+            }
+            return ciag;
+        }
+
+        public static string ThueMorse(int n)
+        {
+            string ciag = MorseStringGen();
+            String rozwiazanie = ciag.Substring(0, n);
+            return rozwiazanie;
+        }
+    }
+}
